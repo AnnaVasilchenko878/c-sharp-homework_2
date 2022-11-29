899a8b906cc0d370e6c6595f7066baa0456d2cbb
Anna Vasilchenko <annavasilcenko482@gmail.com>
2022-11-29 14:49:11 +0300

    Добавляет оставшиеся задачи

---
+++ third_number/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 99)
+{
+  Console.WriteLine("В вашем числе нет третьей цифры");
+}
+else
+{
+  while (number > 999)
+  {
+    number = number / 10;
+  }
+  Console.WriteLine(number % 10);
+}
+
